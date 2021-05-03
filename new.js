85550eb44437e1490623fe24b27a4cbbbf55c16f
দিব্য দত্ত <dibbyadut@gmail.com>
2021-05-03 19:59:33 +0530

    Create new.js

---
+++ new.js
@@ -0,0 +1,10 @@
+function Copy(){
+   copyText = document.getElementById("result");
+   copyText.select();
+   document.execCommand("copy");
+   alert(copyText,value + "প্ৰতিলিপি কৰা হ'ল।")}
+function Result(){
+   var str = document.getElementById("myform").elements.item(0).value;
+   var result = str.replace(/\m/g,"।").replace(/\n/g,"ৎ");
+document.getElementById("result").innerHTML = result;
+}
